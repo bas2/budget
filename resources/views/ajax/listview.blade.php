4f12c1c68d95d0e833cc11ca1fe703708e4728d1
bas2 <me@bashir.biz>
2017-03-01 01:29:36 +0000

    Add days to column. Number format incoming, outgoing and running balance.

--- resources/views/ajax/listview.blade.php
+++ resources/views/ajax/listview.blade.php
@@ -14,14 +14,14 @@
   <div title2="1" id="rw{{ $budgetrow->id }}" class="cellrow prevdayscol">
   <div style="clear:both"></div>
   <div class="celldate">{{ \Carbon\Carbon::parse($budgetrow->date)->format('d/m/Y') }}</div>
-  <div class="cellmorder">-</div>
+  <div class="cellmorder">{{ \Carbon\Carbon::parse($budgetrow->date)->diff(\Carbon\Carbon::now())->days }}</div>
   <div class="celldescr">{{ $budgetrow->description }}</div>
-  <div class="cellin r">{{ $budgetrow->incoming }}</div>
-  <div class="cellout r">{{ $budgetrow->outgoing }}</div>
+  <div class="cellin r">{{ number_format($budgetrow->incoming,2) }}</div>
+  <div class="cellout r">{{ number_format($budgetrow->outgoing,2) }}</div>
   <div class="cellbal r">
   {{ $runbal += $budgetrow->incoming -= $budgetrow->outgoing }}
   </div>
-  <div class="cellsav r">0</div>
+  <div class="cellsav r">{{ $runbal + \App\Saving::getLastEntry('runbal') }}</div>
   <div class="cellnotes">{{ $budgetrow->notes }}&nbsp;</div>
   </div>
 @endforeach
