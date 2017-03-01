2b6b6bdda92ddd46afb7cde368c6485424d714d3
bas2 <me@bashir.biz>
2017-03-01 01:40:21 +0000

    Added a getLastEntry column method.

--- app/Saving.php
+++ app/Saving.php
@@ -4,9 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Saving extends Model
+class Saving extends Account
 {
   public static $account_number = '24362957'; # ISA: 
 
-  
+  public static function getLastEntry($column) {
+    $select=self::latest('date')->latest('id')->get();
+    if ($column=='amount') {
+      $incoming  = ($select[0]->outgoing=='0.00' && $select[0]->incoming!='0.00') ? 1 : 0;
+      $outgoing  = ($select[0]->outgoing!='0.00' && $select[0]->incoming=='0.00') ? 1 : 0;
+      if (!$incoming && !$outgoing) {
+        $incoming = 0;
+        $outgoing = 1;
+      } // End if.
+      return ($incoming) ? $select[0]->incoming : $select[0]->outgoing ;
+    } else {
+    return $select[0]->$column;
+    }
+  }
 }
