36ca21d2af5e03c73deae21e69f32082630ee1b9
bas2 <me@bashir.biz>
2017-04-23 19:29:30 +0100

    Added form-group class around top row of elements. Put in/out radios in top row. Put notes in a form-group.

--- resources/views/ajax/getrow.blade.php
+++ resources/views/ajax/getrow.blade.php
@@ -1,5 +1,5 @@
 {{-- // Retrieves row to display in 'Edit row' section: --}}
-
+<div class="form-group">
 {!! Form::select('code', $codes, $editrows['code']) !!}
 
 {!! Form::date('date', $editrows['date'], ['class'=>'form-control','id'=>'date','title'=>'date']) !!}
@@ -8,12 +8,13 @@
 
 {!! Form::text('amount', number_format($editrows['amount'],2), ['class'=>'form-control','title'=>'amount']) !!}
 
-{!! Form::text('notes', $editrows['notes'], ['class'=>'form-control','title'=>'notes']) !!}
-
-<div class="form-group">
 {!! Form::label('in',Form::radio('in_out', 'in', $editrows['incoming'], ['id'=>'in']).'In',['class'=>'radio-inline'],false ) !!}
 
 {!! Form::label('out',Form::radio('in_out', 'out', $editrows['outgoing'], ['id'=>'out']).'Out',['class'=>'radio-inline'],false ) !!}
+</div>
+
+<div class="form-group">
+{!! Form::text('notes', $editrows['notes'], ['class'=>'form-control','title'=>'notes']) !!}
 </div>
 
 {!! Form::submit('Update >', ['class'=>'btn btn-primary btn-block','id'=>'editdata']) !!}
