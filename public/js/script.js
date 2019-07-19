70d507e9b99b0297d59b960150a97fb251ed5f2b
Bashir <bashirpatel73@gmail.com>
2019-07-19 10:59:21 +0100

    Removed comments.

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,15 @@
-//Cufon.replace('#cellhead, fieldset legend'); // Works without a selector engine
-               
-/* English/UK initialisation for the jQuery UI date picker plugin. */
-/* Written by Stuart. */
-jQuery(function($){
-  $.datepicker.regional['en-GB'] = {
-      closeText: 'Done',
-      prevText: 'Prev',
-      nextText: 'Next',
-      currentText: 'Today',
-      monthNames: ['January','February','March','April','May','June',
-        'July','August','September','October','November','December'],
-  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
-  weekHeader: 'Wk',
-  dateFormat: 'dd/mm/yy',
-  firstDay: 1,
-  isRTL: false,
-  showMonthAfterYear: false,
-  yearSuffix: ''};
-  $.datepicker.setDefaults($.datepicker.regional['en-GB']);
-});
 
 function handleupdown(datemore) {
   // Handle up/down buttons:
   // Disable both:
   $('#btnUp,#btnDown').attr('disabled','disabled');
   
-  //var datemore = str; // e.g. 0-1-1 or 0
-  //alert(datemore);
   if (datemore!=0) {
     var splitdatemore = datemore.split('-');
     var minmorder     = splitdatemore[0]; 
     var curmorder     = splitdatemore[1];
     var maxmorder     = splitdatemore[2];
-    //alert(minmorder+' '+curmorder+' '+maxmorder);
+
     if (minmorder==curmorder) { // Enable down
       $('#btnDown').removeAttr('disabled');
     } else if (maxmorder==curmorder) { // Enable up
