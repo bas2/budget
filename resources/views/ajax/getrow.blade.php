8be32f521f121e02139e2d4de190bde5ba93fbfe
Bashir <bashirpatel73@gmail.com>
2019-07-27 18:45:49 +0100

    In Out radio on own row. Notes block.

--- resources/views/ajax/getrow.blade.php
+++ resources/views/ajax/getrow.blade.php
@@ -7,14 +7,16 @@
 {!! Form::text('descr', $editrows['descr'], ['class'=>'form-control','title'=>'description']) !!}
 
 {!! Form::text('amount', number_format($editrows['amount'],2), ['class'=>'form-control','title'=>'amount']) !!}
+</div>
 
+<div class="form-group">
 {!! Form::label('in',Form::radio('in_out', 'in', $editrows['incoming'], ['id'=>'in']).'In',['class'=>'radio-inline'],false ) !!}
 
 {!! Form::label('out',Form::radio('in_out', 'out', $editrows['outgoing'], ['id'=>'out']).'Out',['class'=>'radio-inline'],false ) !!}
 </div>
 
 <div class="form-group">
-{!! Form::text('notes', $editrows['notes'], ['class'=>'form-control','title'=>'notes']) !!}
+{!! Form::text('notes', $editrows['notes'], ['class'=>'form-control text-block','title'=>'notes']) !!}
 </div>
 
 {!! Form::submit('Update >', ['class'=>'btn btn-primary btn-block','id'=>'editdata']) !!}
