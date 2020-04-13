0fdc2d4a265aee48df09e6cdab750eba954a4a11
Bashir <bashirpatel73@gmail.com>
2020-04-13 17:43:14 +0100

    Removed Total column, days to changed.

--- resources/views/ajax/listview.blade.php
+++ resources/views/ajax/listview.blade.php
@@ -7,20 +7,20 @@
     <th class="text-right">IN</th>
     <th class="text-right">OUT</th>
     <th class="text-right">Balance</th>
-    <th class="text-right">Total</th>
+    <!-- <th class="text-right">Total</th> -->
     <th>Notes</th>
   </tr>
   </thead>
   <tbody>
   @foreach($rows as $budgetrow)
     <tr{{ ((++$rowcounter==1)?' title2=1':'') }} id="rw{{ $budgetrow->id }}" class="cellrow">
-      <td>{{ \Carbon\Carbon::parse($budgetrow->date)->format('d/m/Y') }}</td>
-      <td>{{ \Carbon\Carbon::parse($budgetrow->date)->diff(\Carbon\Carbon::now())->days }}</td>
+      <td>({{ \Carbon\Carbon::parse($budgetrow->date)->format('D') }}){{ \Carbon\Carbon::parse($budgetrow->date)->format('d/m/Y') }}</td>
+      <td>{{ \Carbon\Carbon::parse(date('Y-m-d'))->diff( \Carbon\Carbon::parse($budgetrow->date) )->days }}</td>
       <td>{{ $budgetrow->description }}</td>
       <td class="text-right">{{ number_format($budgetrow->incoming,2) }}</td>
       <td class="text-right">{{ number_format($budgetrow->outgoing,2) }}</td>
       <td class="text-right">{{ number_format( ($runbal += $budgetrow->incoming -= $budgetrow->outgoing),2) }}</td>
-      <td class="text-right">{{ number_format( ($runbal + \App\Saving::getLastEntry('runbal')),2 ) }}</td>
+      <!-- <td class="text-right">{{ number_format( ($runbal + \App\Saving::getLastEntry('runbal')),2 ) }}</td> -->
       <td>{{ $budgetrow->notes }}&nbsp;</td>
     </tr>
   @endforeach
