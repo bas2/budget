6efabb3e505f2ac624294bb0eb62a751742fd8fa
bas2 <bashirpatel73@gmail.com>
2020-09-01 22:39:10 +0100

    Added colour to different rows.

--- resources/views/ajax/listview.blade.php
+++ resources/views/ajax/listview.blade.php
@@ -13,7 +13,11 @@
   </thead> -->
   <tbody>
   @foreach($rows as $budgetrow)
-    <tr{{ ((++$rowcounter==1)?' title2=1':'') }} id="rw{{ $budgetrow->id }}" class="cellrow">
+    <tr{{ ((++$rowcounter==1)?' title2=1':'') }} id="rw{{ $budgetrow->id }}"
+     class="cellrow{{ (($budgetrow->id==2912)?' hl2':'') }}
+     {{ (( round( ( time() - strtotime(\Carbon\Carbon::parse($budgetrow->date)) )/(60*60*24) ) >=0)?' hl3':'') }}
+     {{ (( ($runbal + $budgetrow->incoming - $budgetrow->outgoing) < 0 )?' hl4':'') }}
+     {{ (( $budgetrow->incoming > 0 )?' hl5':'') }}">
       <td class="text-right" nowrap="nowrap">{{ \Carbon\Carbon::parse($budgetrow->date)->format('D j M') }}</td>
       <td class="text-right">{{ \Carbon\Carbon::parse(date('Y-m-d'))->diff( \Carbon\Carbon::parse($budgetrow->date) )->days }}</td>
       <td>{{ $budgetrow->description }}</td>
